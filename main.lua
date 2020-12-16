9d396802499b01bfed5db1eb2202e7384b3b7563
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2020-12-16 13:20:25 +0100

    add alot of stuff

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- > init
-local lib = loadfile("ui.lua")()
-local functions = loadfile("functions.lua")()
-local variables = loadfile("variables.lua")()
+local lib = loadfile("uranium/ui.lua")()
+local functions = loadfile("uranium/functions.lua")()
+local variables = loadfile("uranium/variables.lua")()
 
 -- > tabs & sections
 
@@ -16,6 +16,10 @@ local PlayerConfigSection = PlayerTab:CreateSection("Config")
 local WorldTab = lib:CreateTab("World")
 local WorldMainSection = WorldTab:CreateSection("World")
 local WorldConfigSection = WorldTab:CreateSection("Config")
+
+local CamerasTab = lib:CreateTab("Cameras")
+local CamerasMainSection = CamerasTab:CreateSection("Cameras")
+local CamerasConfigSection = CamerasTab:CreateSection("Config")
 
 local RenderTab = lib:CreateTab("Render")
 local RenderMainSection = RenderTab:CreateSection("Render")
@@ -95,6 +99,26 @@ WeaponsConfigSection:CreateToggle("No Spread", function( arg1 )
     variables.Settings.Weapons.NoSpread = arg1
 end)
 
+--// player
+
+PlayerMainSection:CreateDropdown("Teleport to", variables.tpNames, 1, function( arg1 )
+	local Cframe = CFrame.new(variables.tpLocations[arg1].X, variables.tpLocations[arg1].Y + 5, variables.tpLocations[arg1].Z) -- i know bad code im fixing it later
+	if variables.Settings.Player.TweenTP then
+		functions.tween(variables.LocalPlayer.Character.HumanoidRootPart, .3,
+			{
+				CFrame = Cframe
+			}
+		)
+	else
+		functions.MoveTo(Cframe)
+	end
+end)
+
+
+PlayerConfigSection:CreateToggle("Tween Teleport", function( arg1 )
+	variables.Settings.Player.TweenTP = arg1
+end)
+
 --// world
 
 WorldMainSection:CreateToggle("Spam CCTV", function( arg1 )
@@ -103,6 +127,40 @@ end)
 
 WorldMainSection:CreateToggle("Spam time", function( arg1 )
 	variables.Settings.World.TimeSpam = arg1
+end)
+
+WorldMainSection:CreateToggle("Spam doors", function( arg1 )
+	variables.Settings.World.DoorSpam = arg1
+end)
+
+WorldMainSection:CreateButton("No minimum level", function( ... )
+	workspace.minimumLevel.Position = Vector3.new(workspace.minimumLevel.Position.X, -9e9, workspace.minimumLevel.Position.Z)
+end)
+
+--// Cameras
+
+CamerasMainSection:CreateButton("Move Cameras Up", function()
+	for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
+		functions.rotateSecCam2(v, 0, 1)
+	end
+end)
+
+CamerasMainSection:CreateButton("Move Cameras Down", function()
+	for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
+		functions.rotateSecCam2(v, 0, -1)
+	end
+end)
+
+CamerasMainSection:CreateButton("Move Cameras Left", function()
+	for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
+		functions.rotateSecCam2(v, 1, 0)
+	end
+end)
+
+CamerasMainSection:CreateButton("Move Cameras Right", function()
+	for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
+		functions.rotateSecCam2(v, -1, 0)
+	end
 end)
 
 -- > render loop
@@ -116,14 +174,20 @@ end)
 variables.RunService:BindToRenderStep("functionLoop", 0, function( ... )
     if variables.Settings.World.SpamCCTV then
 		for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
-			functions.rotateSecCam(v, math.random(1000), math.random(-1000))
+			functions.rotateSecCam(v)
 		end
 	end
 
 	if variables.Settings.World.TimeSpam then
 		for i,v in pairs(workspace.AllMovables.SecurityCams:GetChildren()) do
-			x = math.random(-1000,1000)
+			x = math.random(-1000, 1000)
             variables.FireServer("FireAllClients","updateClock",x)
+		end
+	end
+
+	if variables.Settings.World.DoorSpam then
+		for i,v in pairs(workspace.AllDoors:GetChildren()) do
+			variables.FireServer("updateDoorSystem", v)
 		end
 	end
 end)
