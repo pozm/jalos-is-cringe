a8aac24bf7b9b8e32b485c1c067c349e0db35e7a
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2020-12-16 12:06:26 +0100

    fix shit again

--- functions.lua
+++ functions.lua
@@ -5,10 +5,10 @@ return {
         variables.FireServer("FireAllClients","drawLaser", origin, target, properties)
     end,
 
-    rotateSecCam = function(camera, val1, val2)
+    rotateSecCam = function(camera)
         if not camera then  return end
-        val1 = val1 * 2
-        val2 = val2 * 2
+        val1 = math.random(-1000,1000) * 2
+        val2 = math.random(-1000,1000) * 2
         variables.FireServer("updateSecurityCamRotation", camera, val1, val2)
     end
 }
