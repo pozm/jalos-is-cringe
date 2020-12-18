e1c8c9d4bb7028a3cd84482ce0897509de0459f7
pozm <44528100+pozm@users.noreply.github.com>
2020-12-18 00:25:37 +0000

    move

--- functions.lua
+++ functions.lua
@@ -73,25 +73,4 @@ return {
 			}
 		end
 	},
-	checkUpdated = function() 
-	
-		local exists = isfile('uranium/version')
-		if (not exists) return false;
-		local lastVersion = readfile('uranium/version')
-		local id = game:GetService("HttpService"):JSONDecode(Get("https://api.github.com/repos/".. rRepo .."/jalos-is-cringe/commits?per_page=1"))[1].sha
-		writefile('uranium/version',id)
-		if (id == lastVersion) return true;
-		return false;
-
-	
-	end
-	Update = function() {
-		writefile("uranium/main.lua", Get(main))
-
-		writefile("uranium/ui.lua", Get(ui))
-
-		writefile("uranium/functions.lua", Get(functions))
-
-		writefile("uranium/variables.lua", Get(variables))
-	}
 }
